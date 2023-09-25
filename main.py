e05c9f63b37cc74e1c50ef61ade22a3c19d5d6c2
visvesh-r48 <145240391+visvesh-r48@users.noreply.github.com>
2023-09-25 21:07:00 +0000

    Created ChatBot

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+
+def chatbot():
+  print("Hello, welcome to the ChatBot.\n")
+  name = input("Please enter your name: \n")
+  age = input("Please enter your age: \n")
+  print(f"Nice to meet you {name}. I am happy to help you with your needs.\n")
+  print("Please choose from the following options:\n")
+  print("1. Option 1\n2. Option 2\n3. Option 3\n4. Exit ChatBot")
+  choice = int(input("Enter the choice number: "))
+
+  if(choice == 4):
+    print("Thank you for using ChatBot. Goodbye.")
+    exit()
+
+chatbot()
+
